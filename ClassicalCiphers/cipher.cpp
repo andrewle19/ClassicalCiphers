7c045ff6aca39661be9bf4d08bc86b0356438f11
Andrew Le <andrew@Andrews-MacBook-Pro.local>
2018-02-20 14:24:58 -0800

    added function to initialize the playfairset/matrix and parse the plain text to prepare it for the playfair cipher

--- ClassicalCiphers/cipher.cpp
+++ ClassicalCiphers/cipher.cpp
@@ -27,6 +27,84 @@ string removeSpace(string plain){
 }
 
 
+// Initialize Matrix of the playfair cipher
+// Input: key - key to encrypt with, playfairset[] - unique set of characters, setsize - size of set
+// playfairMatrix- the matrix u want to initialize
+// OUTPUT: NONE:
+void initMatrix(string key, char playfairset[],int setsize, char playfairMatrix[][5]) {
+    bool found = false;
+    int setcount = 0;
+    // loop through the plain text and insert into set
+    for(int i = 0; i < key.length(); i++) {
+        
+        found = false; // set default found case to false;
+        
+        // loop throught the set to see if the alphabetical character is in the set already
+        for(int j = 0; j < setsize; j++) {
+            
+            // since j and i are the same set found as true if it is found in the set
+            if(key[i] == 'j' || key[i] == 'i'){
+                if(playfairset[j] == 'i' || playfairset[j] == 'j'){
+                    found = true;
+                }
+            }
+            
+            //  if plain character is found in the set. set found as true
+            else if (playfairset[j] == key[i]) {
+                found = true;
+            }
+            
+        }
+        
+        // if alphabetical is not found add the plain text alpha to set
+        if(!found){
+            playfairset[setcount] = key[i];
+            setcount++; // increment set count
+        }
+    }
+    
+    // fill in the rest of the alphabetical characters for matrix
+    char alpha = 'a';
+    while(setcount != setsize){
+        found = false;
+        // loop throught the set to check if that alpha is being used already
+        for(int i = 0; i < setcount; i++) {
+            
+            // since i and j are the same if either are found set it as true
+            if(alpha == 'i' || alpha == 'j'){
+                if(playfairset[i] == 'i' || playfairset[i] == 'j'){
+                    found = true;
+                }
+            }
+            
+            //  if plain character is found in the set. set found as true
+            if(playfairset[i] == alpha){
+                found = true;
+            }
+        }
+        
+        // if alphabetical is not found add the plain text alpha to set
+        if(!found){
+            playfairset[setcount] = alpha;
+            setcount++; // increment set account
+        }
+        
+        alpha++; // increment the alpha after we check the whole matrix for it
+    }
+    
+    
+    // INITIALIZE THE PLAYFAIR MATRIX AFTER THE SET IS ready
+    int setindex = 0; // start the set index at 0
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            playfairMatrix[i][j] = playfairset[setindex];
+            setindex++;
+        }
+    }
+}
+
+
+
 int main(int argc, char** argv)
 {
 	/**
@@ -55,83 +133,63 @@ int main(int argc, char** argv)
     if(string(argv[1]) == "PLF"){
         
         
-        string plain = "hello world";
-        // remove the white space
-        plain = removeSpace(plain);
+        string key = "monarchy";
         
-        bool found; // to check if the alphabetical character is in set
-        int setcount = 0;
+        // remove the white space
+        key = removeSpace(key);
+        
         int setsize = 25;
 
         char playfairMatrix[5][5]; // matrix of he playfair cipher
         char playfairset[setsize]; // set that holds unique characters
         
+        initMatrix(key, playfairset, setsize,playfairMatrix);
         
-        // loop through the plain text and insert into set
-        for(int i = 0; i < plain.length(); i++) {
-            
-            found = false; // set default found case to false;
-            
-            // loop throught the set to see if the alphabetical character is in the set already
-            for(int j = 0; j < setsize; j++) {
-                
-                // since j and i are the same set found as true if it is found in the set
-                if(plain[i] == 'j' || plain[i] == 'i'){
-                    if(playfairset[j] == 'i' || playfairset[j] == 'j'){
-                        found = true;
-                    }
-                }
-                
-                //  if plain character is found in the set. set found as true
-                else if (playfairset[j] == plain[i]) {
-                    found = true;
-                }
-                
+        
+        
+        // print the matrix
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                cout << playfairMatrix[i][j] << ' ';
             }
-            
-            // if alphabetical is not found add the plain text alpha to set
-            if(!found){
-                playfairset[setcount] = plain[i];
-                setcount++; // increment set count
-            }
-          }
-        
-        // fill in the rest of the alphabetical characters for matrix
-        char alpha = 'a';
-        while(setcount != setsize){
-            found = false;
-            // loop throught the set to check if that alpha is being used already
-            for(int i = 0; i < setcount; i++) {
-                
-                // since i and j are the same if either are found set it as true
-                if(alpha == 'i' || alpha == 'j'){
-                    if(playfairset[i] == 'i' || playfairset[i] == 'j'){
-                        found = true;
-                    }
-                }
-                
-                //  if plain character is found in the set. set found as true
-                if(playfairset[i] == alpha){
-                    found = true;
-                }
-            }
-            
-            // if alphabetical is not found add the plain text alpha to set
-            if(!found){
-                playfairset[setcount] = alpha;
-                setcount++; // increment set account
-            }
-            
-            alpha++; // increment the alpha after we check the whole matrix for it
-        }
-        
-        
-        
-        for(int i = 0; i < setcount; i++){
-            cout << playfairset[i] << ' ';
+            cout << '\n';
         }
         cout << '\n';
         
+        
+        // ENCRYPT A Key
+        string plain = "balloonk";
+        plain = removeSpace(plain);
+        string newplain;
+        cout << plain.length() << endl;
+        
+        // Preparing the plain text for playfair cipher
+        for(int i = 0; i < plain.length(); i += 2){
+            
+            // add x inbetween same letter characters
+            if(plain[i] == plain[i+1]){
+                newplain += plain[i];
+                newplain += 'x';
+                --i;
+            } else {
+                
+                if(isalpha(plain[i+1])){
+                    newplain += plain[i];
+                    newplain += plain[i+1];
+                }
+                else {
+                    newplain += plain[i];
+                }
+                
+            }
+        }
+       
+        // if its uneven add a character
+        if(newplain.length()%2 != 0) {
+            newplain += 'x';
+        }
+    
+        cout << newplain << endl;
         
         
     }
