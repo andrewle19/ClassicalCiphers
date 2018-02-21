7a9568bf3504dff94678893b0be4be3f3c212e95
Andrew Le <andrew@Andrews-MacBook-Pro.local>
2018-02-20 23:20:10 -0800

    Added encrypt and decrypt functions need to make it modular

--- ClassicalCiphers/cipher.cpp
+++ ClassicalCiphers/cipher.cpp
@@ -9,6 +9,12 @@
 #include <locale>
 using namespace std;
 
+// Struct to store the matrix location of the plain text
+struct matrixLocation{
+    int row;
+    int col;
+};
+
 // Function to remove white space from plain text
 // Input: plain text
 // output: plain text without white space
@@ -104,6 +110,140 @@ void initMatrix(string key, char playfairset[],int setsize, char playfairMatrix[
 }
 
 
+// prepares the plain text for playfair cipher
+//by separating same characters with xs and making sure sting is even
+// input: plain- plain text to insert
+// output: returns new string
+string preparePlainText(string plain){
+    string newplain;
+    // Preparing the plain text for playfair cipher
+    for(int i = 0; i < plain.length(); i += 2){
+        
+        // add x inbetween same letter characters
+        if(plain[i] == plain[i+1]){
+            newplain += plain[i];
+            newplain += 'x';
+            --i;
+        } else {
+            // checks if next character is an alpha if it is then add both
+            if(isalpha(plain[i+1])){
+                newplain += plain[i];
+                newplain += plain[i+1];
+            }
+            else {
+                newplain += plain[i];
+            }
+        }
+    }
+    
+    // if its uneven add a character
+    if(newplain.length() % 2 != 0) {
+        newplain += 'x';
+    }
+    
+    return newplain;
+}
+
+// Helper function for main encryption function
+// playfairMatrix-The encryption matrix,
+//first letter: first letter to search for, secondletter; 2nd letter to search for
+// OUTPUT: new cipher text
+string encryptHelper(char playfairMatrix[][5],char firstletter, char secondletter) {
+    
+    matrixLocation one; // first letter location
+    matrixLocation two; // second letter location
+    string ciphertext;
+    
+    // Finds the location of the two letters
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if(firstletter == playfairMatrix[i][j]){
+                one.row = i;
+                one.col = j;
+            } else if(secondletter == playfairMatrix[i][j]){
+                two.row = i;
+                two.col = j;
+            }
+        }
+    }
+    
+    // ENCRYPT THE PLAIN TEXT TO CIPHER TEXT
+    
+    // checks if each are in the same column moves down row
+    if(one.col == two.col){
+        ciphertext += playfairMatrix[one.row+1%5][one.col];
+        ciphertext += playfairMatrix[two.row+1%5][two.col];
+    }
+    // checks if in same row moves right one column
+    else if(one.row == two.row){
+        ciphertext += playfairMatrix[one.row][one.col+1%5];
+        ciphertext += playfairMatrix[two.row][two.col+1%5];
+    }
+    // if not in row or col will swap the two columns but not the rows
+    else {
+        ciphertext += playfairMatrix[one.row][two.col];
+        ciphertext += playfairMatrix[two.row][one.col];
+    }
+    
+    return ciphertext;
+}
+
+// encript a plain text message into cipher text
+string encrypt(char playfairMaxtrix[][5],string plain){
+    string ciphertext;
+    for(int i = 0; i < plain.length(); i+=2){
+        ciphertext += encryptHelper(playfairMaxtrix, plain[i], plain[i+1]);
+    }
+    return ciphertext;
+}
+
+string decryptHelper(char playfairMatrix[][5],char firstletter, char secondletter){
+    matrixLocation one; // first letter location
+    matrixLocation two; // second letter location
+    string plain;
+    
+    // Finds the location of the two letters
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if(firstletter == playfairMatrix[i][j]){
+                one.row = i;
+                one.col = j;
+            } else if(secondletter == playfairMatrix[i][j]){
+                two.row = i;
+                two.col = j;
+            }
+        }
+    }
+    
+    // ENCRYPT THE PLAIN TEXT TO CIPHER TEXT
+    
+    // checks if each are in the same column moves up a row
+    if(one.col == two.col){
+        plain += playfairMatrix[one.row-1%5][one.col];
+        plain += playfairMatrix[two.row-1%5][two.col];
+    }
+    // checks if in same row moves right one column
+    else if(one.row == two.row){
+        plain += playfairMatrix[one.row][one.col-1%5];
+        plain += playfairMatrix[two.row][two.col-1%5];
+    }
+    // if not in row or col will swap the two columns but not the rows
+    else {
+        plain += playfairMatrix[one.row][two.col];
+        plain += playfairMatrix[two.row][one.col];
+    }
+    
+    return plain;
+}
+
+// encript a plain text message into cipher text
+string decrypt(char playfairMaxtrix[][5],string cipher){
+    string plain;
+    for(int i = 0; i < cipher.length(); i+=2){
+        plain += decryptHelper(playfairMaxtrix, cipher[i], cipher[i+1]);
+    }
+    return plain;
+}
 
 int main(int argc, char** argv)
 {
@@ -158,39 +298,18 @@ int main(int argc, char** argv)
         
         
         // ENCRYPT A Key
-        string plain = "balloonk";
+        string plain = "balloon";
+        string ciphertext;
         plain = removeSpace(plain);
-        string newplain;
-        cout << plain.length() << endl;
+        plain = preparePlainText(plain);
         
-        // Preparing the plain text for playfair cipher
-        for(int i = 0; i < plain.length(); i += 2){
-            
-            // add x inbetween same letter characters
-            if(plain[i] == plain[i+1]){
-                newplain += plain[i];
-                newplain += 'x';
-                --i;
-            } else {
-                
-                if(isalpha(plain[i+1])){
-                    newplain += plain[i];
-                    newplain += plain[i+1];
-                }
-                else {
-                    newplain += plain[i];
-                }
-                
-            }
-        }
-       
-        // if its uneven add a character
-        if(newplain.length()%2 != 0) {
-            newplain += 'x';
-        }
-    
-        cout << newplain << endl;
+        cout << plain << endl;
         
+        ciphertext = encrypt(playfairMatrix, plain);
+        cout << ciphertext << endl;
+        ciphertext = decrypt(playfairMatrix, ciphertext);
+        cout << ciphertext << endl;
+     
         
     }
     // row transposition selection
